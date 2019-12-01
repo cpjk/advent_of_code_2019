7a41cb964c960ad64010f9210b45c908c8fce595
Chris Kelly <chris.kelly@shopify.com>
2019-12-01 13:18:21 -0500

    Finish day 1

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
-import "fmt"
 import "github.com/cpjk/advent_of_code_2019/one"
 
 func main() {
-	fmt.Println("Hello!")
 	one.One()
 }
--- one/one.go
+++ one/one.go
@@ -5,35 +5,17 @@ import "log"
 
 import "github.com/cpjk/advent_of_code_2019/util"
 
-/*
-Fuel required to launch a given module is based on its mass.
-Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
-
-For example:
-
-For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-For a mass of 1969, the fuel required is 654.
-For a mass of 100756, the fuel required is 33583.
-The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
-
-What is the sum of the fuel requirements for all of the modules on your spacecraft?
-*/
+// https://adventofcode.com/2019/day/1
+// https://adventofcode.com/2019/day/1#part2
 
 func One() {
 	moduleMassesInt, err := util.InjestIntegerList("input.txt")
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(moduleMassesInt)
 
 	totalFuel := totalFuelForModules(moduleMassesInt)
-	fmt.Println("total fuel: ", totalFuel)
-}
-
-func fuelForModule(moduleMass int) int {
-	fuel := (moduleMass / 3) - 2
-	return fuel
+	fmt.Println("Day 1 answer: ", totalFuel)
 }
 
 func totalFuelForModules(masses []int) int {
@@ -44,4 +26,29 @@ func totalFuelForModules(masses []int) int {
 
 	return totalFuel
 }
+
+func fuelForModule(moduleMass int) int {
+	return fuelForMass(moduleMass)
+}
+
+func fuelForMass(mass int) int {
+	return doFuelForMass(mass, 0)
+}
+
+// Recursively build up the fuel required for the input mass,
+// memoizing the current sum in currentFuel
+func doFuelForMass(mass int, currentFuel int) int {
+	if mass <= 0 {
+		return currentFuel
+	}
+
+	newFuelMass := (mass / 3) - 2
+	if newFuelMass <=0 {
+		return doFuelForMass(newFuelMass, currentFuel)
+	}
+
+	newFuelTotal := newFuelMass + currentFuel
+	return doFuelForMass(newFuelMass, newFuelTotal)
+}
+
 
